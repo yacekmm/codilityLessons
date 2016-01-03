83beafd6446305543f538d62f18a22b6cac65203
Jacek Milewski <jacek.milewski@orange.com>
2016-01-03 12:53:34 +0100

    missingInteger ready

---
+++ src/test/groovy/com/sollers/test/lessons/countingelements/MissingIntegerSpec.groovy
@@ -0,0 +1,27 @@
+package com.sollers.test.lessons.countingelements
+
+import com.sollers.lessons.countingelements.MissingInteger
+import spock.lang.Specification
+
+class MissingIntegerSpec extends Specification {
+
+    MissingInteger sut = new MissingInteger()
+
+    def "Solution"() {
+        expect:
+        sut.solution(A as int[]) == result
+
+        where:
+        A                           |   result
+        [1, 3, 6, 4, 1, 2]          |   5
+        [1, 3, 5, 4, 1, 2]          |   6
+        [6, 3, 5, 4, 6, 2]          |   1
+        [6, 3, 5, 4, 1, -2]         |   2
+        [-2]                        |   1
+        [2]                         |   1
+        [222]                       |   1
+        [1, 222]                    |   2
+        [0, 1]                      |   2
+        [1, 2]                      |   3
+    }
+}

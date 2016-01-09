e0a6c9ce45d412e20e032b3a6ae8a63562d6780c
Jacek Milewski <jacek.milewski.k@gmail.com>
2016-01-09 20:40:07 +0100

    StoneWall 35%

---
+++ src/main/java/com/codility/lessons/stacksandqueues/StoneWall.java
@@ -0,0 +1,51 @@
+package com.codility.lessons.stacksandqueues;
+
+import java.util.Stack;
+
+public class StoneWall {
+
+    public int solution(int[] H){
+
+        Stack<Integer> stack = new Stack<>();
+        int recentHeight = -1;
+        int result = 0;
+
+        for (int i = 0; i < H.length; i++) {
+            int currentHeight = H[i];
+
+            if(recentHeight < 0){
+                stack.push(currentHeight);
+                recentHeight = currentHeight;
+                result++;
+            }else if(recentHeight == currentHeight){
+                //do nothing
+            }else if(currentHeight > recentHeight){
+                int blockToPush = currentHeight - recentHeight;
+                stack.push(blockToPush);
+                recentHeight = currentHeight;
+                result++;
+            }else if(currentHeight < recentHeight){
+                Integer popped = stack.pop();
+                if(recentHeight == currentHeight - popped){
+                    // ???
+                }else if( recentHeight - currentHeight < popped){
+                    stack.push(popped);
+                    stack.push(recentHeight - currentHeight);
+                    result++;
+                }else if(recentHeight - currentHeight > popped){
+                    stack.push(currentHeight);
+                    result++;
+                }
+                recentHeight = currentHeight;
+            }
+
+            if(currentHeight < recentHeight){
+                result++;
+                recentHeight = currentHeight;
+                stack.pop();
+            }
+        }
+
+        return result;
+    }
+}

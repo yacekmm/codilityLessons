f344eb9c1ccc3a88c6ca736dcdfa2a9b40ac83e3
Jacek Milewski <jacek.milewski@orange.com>
2016-02-17 18:09:37 +0100

    implementation stub

--- src/main/java/com/codility/lessons/fibonaccinumbers/FibFrog.java
+++ src/main/java/com/codility/lessons/fibonaccinumbers/FibFrog.java
@@ -3,6 +3,51 @@ package com.codility.lessons.fibonaccinumbers;
 public class FibFrog {
 
     public int solution(int[] A){
-        return 0;
+        int result = 0;
+        int distanceToPrevious = 0;
+        int currentPosition = -1;
+
+        if(A.length == 0){
+            return -1;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            distanceToPrevious++;
+            if(A[i] == 1){
+                if(isFibonacci(distanceToPrevious)){
+                    currentPosition = i;
+                    distanceToPrevious = 0;
+                    result++;
+                }else{
+                    continue;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public boolean isFibonacci(long number) {
+        int n = 0;
+        while(true){
+            long fibonacci = countFibonacci(n);
+            if(number == fibonacci){
+                return true;
+            }else if(fibonacci > number){
+                return false;
+            }
+            n++;
+        }
+    }
+
+    public long countFibonacci(int n){
+        double a, b, c;
+
+        a = Math.pow( (1.0 + Math.sqrt(5d)) / 2d, n);
+        b = Math.pow( (1.0 - Math.sqrt(5d)) / 2d, n);
+
+        return Math.round((a - b) / Math.sqrt(5d));
     }
 }
+
+//0 1 1 2 3 5 8 13 21 34
--- src/test/groovy/com/codility/test/lessons/fibonaccinumbers/FibFrogSpec.groovy
+++ src/test/groovy/com/codility/test/lessons/fibonaccinumbers/FibFrogSpec.groovy
@@ -1,9 +1,60 @@
 package com.codility.test.lessons.fibonaccinumbers
 
+import com.codility.lessons.fibonaccinumbers.FibFrog
 import spock.lang.Specification
 
 class FibFrogSpec extends Specification {
     def "Solution"() {
+        expect:
+        new FibFrog().solution(A as int[]) == res
 
+        where:
+        res |   A
+        -1  |   []
+        1   |   [1]
+//        -1  |   [0]
+        3   |   [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]
     }
+
+    def "test Fibonacci"(){
+        expect:
+        new FibFrog().countFibonacci(n) == res
+
+        where:
+        n   | res
+        0   | 0
+        1   | 1
+        2   | 1
+        3   | 2
+        4   | 3
+        5   | 5
+        6   | 8
+        7   | 13
+    }
+
+    def "is Fibonacci"(){
+        expect:
+        new FibFrog().isFibonacci(n) == res
+
+        where:
+        res    | n
+        true   | 0
+        true   | 1
+        true   | 1
+        true   | 2
+        true   | 3
+        true   | 5
+        true   | 8
+        true   | 13
+        false  | 4
+        false  | 6
+        false  | 7
+        false  | 9
+        false  | 10
+        false  | 11
+        false  | 12
+        false  | 14
+    }
+
+
 }

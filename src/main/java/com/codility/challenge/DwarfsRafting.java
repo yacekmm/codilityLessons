2b5469ef572989f408f108cfdf51d5a325f3937e
Jacek Milewski <jacek.milewski.k@gmail.com>
2016-04-05 20:10:48 +0200

    getIntPosition

--- src/main/java/com/codility/challenge/DwarfsRafting.java
+++ src/main/java/com/codility/challenge/DwarfsRafting.java
@@ -1,8 +1,40 @@
 package com.codility.challenge;
 
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
 public class DwarfsRafting {
 
-    public int solution(){
+    private static final int IMPOSSIBLE_RESULT = -1;
+    private static final String POSITIONS_SEPARATOR = " ";
+    private static final List<String> alphabet =
+            Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+
+    public int solution(int N, String S, String T){
+
+        String[] barrelsPositions = S.split(POSITIONS_SEPARATOR);
+        String[] dwarfsPositions = T.split(POSITIONS_SEPARATOR);
+
+        int freeSitsInLeftFront = getFreeSits(1, N/2, 1, N/2, barrelsPositions, dwarfsPositions);
+        int dwarfsCountInLeftFront = getDwarfsCount(1, N/2, 1, N/2, dwarfsPositions);
+
+        return IMPOSSIBLE_RESULT;
+    }
+
+    public int getDwarfsCount(int rowStart, int rowEnd, int colStart, int colEnd, String[] dwarfsPositions) {
         return 0;
+    }
+
+    private int getFreeSits(int rowStart, int rowEnd, int colStart, int colEnd, String[] barrelsPositions, String[] dwarfsPositions) {
+        return 0;
+    }
+
+    public Point getIntPosition(String stringPosition){
+        String row = stringPosition.substring(0, stringPosition.length() - 1);
+        String col = stringPosition.substring(stringPosition.length() - 1, stringPosition.length());
+
+        return new Point(Integer.parseInt(row), alphabet.indexOf(col) + 1);
     }
 }
--- src/test/groovy/com/codility/test/challenge/DwarfsRaftingSpec.groovy
+++ src/test/groovy/com/codility/test/challenge/DwarfsRaftingSpec.groovy
@@ -3,13 +3,37 @@ package com.codility.test.challenge
 import com.codility.challenge.DwarfsRafting
 import spock.lang.Specification
 
+import java.awt.*
+
 class DwarfsRaftingSpec extends Specification {
     def "test solution"() {
         expect:
-        new DwarfsRafting().solution() == result
+        new DwarfsRafting().solution(N, S, T) == result
 
         where:
-        result  |
+        result  | N     | S     | T
+        4       | 2     | ""    | ""
+    }
 
+    def "test getDwarfsCount"() {
+        expect:
+        new DwarfsRafting().getDwarfsCount(rowStart, rowEnd, colStart, colEnd, dwarfsPos) == result
+
+        where:
+        result  | rowStart  | rowEnd    | colStart  | colEnd    | dwarfsPos
+        0       | 1         | 1         | 1         | 1         | ""
+    }
+
+
+    def "test getIntPosition"() {
+        expect:
+        new DwarfsRafting().getIntPosition(stringPos) == result
+
+        where:
+        result          | stringPos
+        new Point(1,1)  | "1A"
+        new Point(1,2)  | "1B"
+        new Point(2,2)  | "2B"
+        new Point(22,2) | "22B"
     }
 }

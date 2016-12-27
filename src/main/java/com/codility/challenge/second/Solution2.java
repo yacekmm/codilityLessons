597b5ac4b2212d2470ad0f39a4a068911f792f97
Jacek Milewski <jacek.milewski@orange.com>
2016-12-27 22:24:57 +0100

    more testcases

--- src/main/java/com/codility/challenge/second/Solution2.java
+++ src/main/java/com/codility/challenge/second/Solution2.java
@@ -1,17 +1,129 @@
 package com.codility.challenge.second;
 
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Solution2 {
 
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public String solution(String S) {
-        return "";
+        String[] strings = S.split("\n");
+
+        Map<String, List<Photo>> counting = Arrays.stream(strings)
+                .map(s -> Photo.fromString(s))
+                .collect(Collectors.groupingBy(Photo::getCity));
+
+        for (String city : counting.keySet()) {
+            List<Photo> photos = counting.get(city);
+            photos.sort((photo1, photo2) -> photo1.dateTime.isAfter(photo2.dateTime) ? 1 : -1);
+
+            Integer count = photos.size();
+            int digits = count.toString().length();
+            for (int i = 0; i < count; i++) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(photos.get(i).city)
+                        .append(String.format("%0" + digits + "d", i+1))
+                        .append(".")
+                        .append(photos.get(i).extension)
+                        .append("\n");
+                photos.get(i).setResult(sb.toString());
+            }
+        }
+
+        Map<String, String> collect = counting.values().stream()
+                .flatMap(x -> x.stream())
+                .collect(Collectors.toMap(Photo::getOriginal, Photo::getResult));
+
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(strings)
+                .map(s -> collect.get(s))
+                .forEach(sb::append);
+
+        return sb.toString().trim();
     }
 
-    public LocalDateTime parseDate(String string) {
-        return LocalDateTime.parse(string, formatter);
+
+    static class Photo {
+        private final String extension;
+        private final String city;
+        private final LocalDateTime dateTime;
+
+
+        private final String original;
+        private String result;
+
+
+        private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        public static Photo fromString(String s) {
+            String[] strings = s.split(", ");
+            return new Photo(
+                    strings[0].substring(s.indexOf(".") + 1, s.indexOf(",")),
+                    strings[1],
+                    parseDate(strings[2]),
+                    s);
+        }
+
+        public Photo(String extension, String city, LocalDateTime dateTime, String original) {
+            this.extension = extension;
+            this.city = city;
+            this.dateTime = dateTime;
+            this.original = original;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getResult() {
+            return result;
+        }
+
+        public String getOriginal() {
+            return original;
+        }
+
+        private static LocalDateTime parseDate(String string) {
+            return LocalDateTime.parse(string, formatter);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Photo photo = (Photo) o;
+
+            if (extension != null ? !extension.equals(photo.extension) : photo.extension != null) return false;
+            if (city != null ? !city.equals(photo.city) : photo.city != null) return false;
+            return dateTime != null ? dateTime.equals(photo.dateTime) : photo.dateTime == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = extension != null ? extension.hashCode() : 0;
+            result = 31 * result + (city != null ? city.hashCode() : 0);
+            result = 31 * result + (dateTime != null ? dateTime.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Photo{" +
+                    "extension='" + extension + '\'' +
+                    ", city='" + city + '\'' +
+                    ", dateTime=" + dateTime +
+                    '}';
+        }
+
+        public void setResult(String result) {
+            this.result = result;
+        }
     }
+
 }
--- src/test/groovy/com/codility/challenge/second/Solution2Spec.groovy
+++ src/test/groovy/com/codility/challenge/second/Solution2Spec.groovy
@@ -2,8 +2,6 @@ package com.codility.challenge.second
 
 import spock.lang.Specification
 
-import java.time.LocalDateTime
-
 class Solution2Spec extends Specification {
     Solution2 solution
 
@@ -16,16 +14,61 @@ class Solution2Spec extends Specification {
         solution.solution(input) == output
 
         where:
-        input | output
-        ""    | ""
+        input                                        | output
+        "a.png, Warsaw, 2016-02-13 13:33:50"         | "Warsaw1.png"
+
+        "a.png, Warsaw, 2016-02-13 13:33:55\n" +
+                "a.png, Warsaw, 2016-02-13 13:33:50" | "Warsaw2.png\nWarsaw1.png"
+
+        "a.png, Warsaw, 2016-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2015-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2014-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2013-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2012-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2011-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2010-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2009-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2008-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2007-02-13 13:33:50" | "Warsaw10.png\n" +
+                "Warsaw09.png\n" +
+                "Warsaw08.png\n" +
+                "Warsaw07.png\n" +
+                "Warsaw06.png\n" +
+                "Warsaw05.png\n" +
+                "Warsaw04.png\n" +
+                "Warsaw03.png\n" +
+                "Warsaw02.png\n" +
+                "Warsaw01.png"
+
+        "a.png, Radom, 2016-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2015-02-13 13:33:50\n" +
+                "a.png, Radom, 2014-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2013-02-13 13:33:50\n" +
+                "a.png, Krakow, 2012-02-13 13:33:50\n" +
+                "a.png, Warsaw, 2011-02-13 13:33:50" |
+                "Radom2.png\n" +
+                "Warsaw3.png\n" +
+                "Radom1.png\n" +
+                "Warsaw2.png\n" +
+                "Krakow1.png\n" +
+                "Warsaw1.png"
     }
 
-    def "parses date"() {
-        expect:
-        solution.parseDate(S) == result
-
-        where:
-        S                     | result
-        "2013-09-05 14:18:15" | LocalDateTime.of(2013, 9, 5, 14, 18, 15)
-    }
+//    def "parses date"() {
+//        expect:
+//        Solution2.Photo.parseDate(S) == result
+//
+//        where:
+//        S                     | result
+//        "2013-09-05 14:18:15" | LocalDateTime.of(2013, 9, 5, 14, 18, 15)
+//    }
+//
+//    def "parses photo"() {
+//        expect:
+//        Solution2.Photo.fromString(input) == result
+//
+//        where:
+//        input                                | result
+//        "a.png, Warsaw, 2016-02-13 13:33:50" | new Solution2.Photo("png", "Warsaw", LocalDateTime.of(2016, 02, 13, 13, 33, 50), s)
+//    }
 }

ed74f9197bddd9724b2351942b3f6a9a4515b72f
Jacek Milewski <jacek.milewski@orange.com>
2016-12-27 20:24:03 +0100

    left right row

--- src/main/java/com/codility/challenge/second/Solution.java
+++ src/main/java/com/codility/challenge/second/Solution.java
@@ -1,8 +1,46 @@
 package com.codility.challenge.second;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
 public class Solution {
 
-    public int solution(int N, String S, String T){
-        return 0;
+    public int solution(int N, String S) {
+        HashSet<String> seatsSet = splitSeats(S);
+        int result = 0;
+
+        for (int i = 1; i <= N; i++) {
+            result += familiesForLeftColumn(i, seatsSet);
+            result += familiesForRightColumn(i, seatsSet);
+        }
+
+        return result;
+    }
+
+    public int familiesForLeftColumn(int row, HashSet<String> seatsSet) {
+        if(seatsSet.contains(row + "A")
+                || seatsSet.contains(row + "B")
+                || seatsSet.contains(row + "C")){
+            return 0;
+        }else {
+            return 1;
+        }
+    }
+
+    public int familiesForRightColumn(int row, HashSet<String> seatsSet) {
+        if(seatsSet.contains(row + "H")
+                || seatsSet.contains(row + "J")
+                || seatsSet.contains(row + "K")){
+            return 0;
+        }else {
+            return 1;
+        }
+    }
+
+    public HashSet<String> splitSeats(String S){
+        return Arrays.stream(S.split(" "))
+                .map(s -> s.toUpperCase())
+                .collect(Collectors.toCollection(HashSet::new));
     }
 }
--- src/test/groovy/com/codility/challenge/second/SolutionSpec.groovy
+++ src/test/groovy/com/codility/challenge/second/SolutionSpec.groovy
@@ -4,13 +4,58 @@ import spock.lang.Specification
 
 class SolutionSpec extends Specification {
 
-    def "Solution returns a result"() {
+    Solution solution
+
+    void setup() {
+        solution = new Solution()
+    }
+
+    def "Solution returns a result for left column"() {
         expect:
-        def solution = new Solution()
-        solution.solution(1, input, "") == result
+        solution.solution(N, S + " 1H + 2H") == result
 
         where:
-        input | result
-        "ew"  | 2
+        N | S       | result
+        1 | ""      | 1
+        1 | "1A"    | 0
+        1 | "1B"    | 0
+        1 | "1C"    | 0
+        1 | "1A 1B" | 0
+        1 | "1A 1C" | 0
+        1 | "1B 1C" | 0
+        1 | "2B 2C" | 1
+        2 | "2B 2C" | 1
+        2 | "1A"    | 1
+    }
+
+    def "Solution returns a result for right column"() {
+        expect:
+        solution.solution(N, S + " 1A + 2A") == result
+
+        where:
+        N | S       | result
+        1 | ""      | 1
+        1 | "1H"    | 0
+        1 | "1J"    | 0
+        1 | "1K"    | 0
+        1 | "1H 1J" | 0
+        1 | "1H 1K" | 0
+        1 | "1J 1K" | 0
+        1 | "2J 2K" | 1
+        2 | "2J 2K" | 1
+        2 | "1H"    | 1
+    }
+
+    def "splits string to hashset"() {
+        expect:
+        solution.splitSeats(S) == result
+
+        where:
+        S       | result
+//        ""      | new HashSet<String>()
+        "1A"    | ["1A"] as HashSet
+        "1A 2B" | ["1A", "2B"] as HashSet
+        "1A 1A" | ["1A"] as HashSet
+        "1a"    | ["1A"] as HashSet
     }
 }
